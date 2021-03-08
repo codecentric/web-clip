3e12b8330286b1606aee6367e6423046431e0650
Angelo Veltens <angelo.veltens@codecentric.de>
2021-03-08 18:31:50 +0100

    add tab infos to popup

--- app/App.js
+++ app/App.js
@@ -12,28 +12,31 @@ const useSession = () => {
 }
 
 function login() {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {type: "doLogin"}, function(response) {
+  chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {type: "doLogin"}, function (response) {
       console.log({response});
     });
   });
 }
 
 function logout() {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {type: "doLogout"}, function(response) {
+  chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {type: "doLogout"}, function (response) {
       console.log({response});
     });
   });
 }
 
-function App() {
-    const session = useSession();
-    return <main>
+function App({tab}) {
+  const session = useSession();
+  return <main>
     <h1>WebTrack</h1>
-      <div>Hello, {session ? session.name : 'world'}.</div>
-      {session ? null : <button onClick={login}>Login</button>}
-      {!session ? null : <button onClick={logout}>Logout</button>}
-    </main>
+    <h2>{tab.title}</h2>
+    <code>{tab.url}</code>
+    <div>Hello, {session ? session.name : 'world'}.</div>
+    {session ? null : <button onClick={login}>Login</button>}
+    {!session ? null : <button onClick={logout}>Logout</button>}
+  </main>
 }
+
 export default App
--- static/index.js
+++ static/index.js
@@ -2,4 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "../app/App";
 import "./index.css";
-ReactDOM.render(<App />, document.querySelector("#root"));
+
+window.onload = function() {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    console.log("active tab", tabs[0]);
+    ReactDOM.render(<App tab={tabs[0]}/>, document.querySelector("#root"));
+  });
+}
+
+
+
