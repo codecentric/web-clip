08f7c63625efcca18e541eecb0ac6e9c0935f04d
Edward Byne <edward.byne@codecentric.de>
2021-03-10 16:21:25 +0100

    Install and configure eslint and prettier

--- src/content/PageContent.js
+++ src/content/PageContent.js
@@ -1,25 +1,37 @@
-import React, {useEffect} from 'react'
+import React from 'react';
 
-import * as rdf from "rdflib";
-import solidNamespace from "solid-namespace";
-import {getDefaultSession, handleIncomingRedirect, login} from "@inrupt/solid-client-authn-browser";
-import {Toolbar} from "./Toolbar";
+import * as rdf from 'rdflib';
+import solidNamespace from 'solid-namespace';
+import { login } from '@inrupt/solid-client-authn-browser';
+import { Toolbar } from './Toolbar';
+import * as PropTypes from 'prop-types';
 
 const ns = solidNamespace(rdf);
-const {sym, st} = rdf;
+const { sym, st } = rdf;
 
 const store = rdf.graph();
-const fetcher = new rdf.Fetcher(store, {fetch: fetch});
-const updater = new rdf.UpdateManager(store)
+const fetcher = new rdf.Fetcher(store, { fetch: fetch });
+const updater = new rdf.UpdateManager(store);
 
 async function loginWithRedirect() {
   await login({
-    oidcIssuer: "https://solidcommunity.net", // TODO: read from plugin configuration?
+    oidcIssuer: 'https://solidcommunity.net', // TODO: read from plugin configuration?
     redirectUrl: window.location.href,
   });
 }
 
+export const PageContent = ({ session }) => {
+  return session ? (
+    <Toolbar webId={session.info.webId} />
+  ) : (
+    <button onClick={loginWithRedirect}>Login</button>
+  );
+};
 
-export const PageContent = ({session}) => {
-  return session ? <Toolbar webId={session.info.webId} /> : <button onClick={loginWithRedirect}>Login</button>
-}
+PageContent.propTypes = {
+  session: {
+    info: {
+      webId: PropTypes.string,
+    },
+  },
+};
--- src/content/Toolbar.js
+++ src/content/Toolbar.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import * as PropTypes from 'prop-types';
 
-export const Toolbar = ({webId}) => (
-    <>
-        {webId}
-        <button>Clip it</button>
-    </>
+export const Toolbar = ({ webId }) => (
+  <>
+    {webId}
+    <button>Clip it</button>
+  </>
 );
+
+Toolbar.propTypes = {
+  webId: PropTypes.string,
+};
