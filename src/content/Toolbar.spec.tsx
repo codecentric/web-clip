34e9c20c7cd756241efc25988309fe9d9d136ea7
Angelo Veltens <angelo.veltens@codecentric.de>
2021-03-12 09:01:44 +0100

    show loading indicator

--- src/content/Toolbar.spec.tsx
+++ src/content/Toolbar.spec.tsx
@@ -2,6 +2,9 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
 import { mockSolidApi } from '../test/solidApiMock';
 import { Toolbar } from './Toolbar';
+import { useProfile } from './useProfile';
+
+jest.mock('./useProfile');
 
 describe('Toolbar', () => {
   const { location } = window;
@@ -15,16 +18,30 @@ describe('Toolbar', () => {
     window.location = location;
   });
 
+  it('renders loading indicator', () => {
+    (useProfile as jest.Mock).mockReturnValue({
+      loading: true,
+      profile: null,
+    });
+    render(<Toolbar />);
+    expect(screen.getByText('Loading...')).toBeInTheDocument();
+  });
+
   it("renders the user's name", () => {
-    const solidApi = mockSolidApi();
-    solidApi.getProfile.mockReturnValue({ name: 'Jane Doe' });
+    (useProfile as jest.Mock).mockReturnValue({
+      loading: false,
+      profile: { name: 'Jane Doe' },
+    });
     render(<Toolbar />);
     expect(screen.getByText('Jane Doe')).toBeInTheDocument();
   });
 
   it("saves a web page to the user's pod", () => {
     const solidApi = mockSolidApi();
-    solidApi.getProfile.mockReturnValue({ name: 'Jane Doe' });
+    (useProfile as jest.Mock).mockReturnValue({
+      loading: false,
+      profile: { name: 'Jane Doe' },
+    });
     window.location.href = 'https://page.example/article';
     window.document.title = 'An interesting article';
     render(<Toolbar />);
--- src/content/Toolbar.tsx
+++ src/content/Toolbar.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import * as PropTypes from 'prop-types';
 import { useSolidApi } from '../api/apiContext';
 import { usePage } from './usePage';
+import { useProfile } from './useProfile';
 
 export const Toolbar = () => {
   const solidApi = useSolidApi();
   const page = usePage();
-  const { name } = solidApi.getProfile();
-  return (
+  const { loading, profile } = useProfile();
+  return loading ? (
+    <p>Loading...</p>
+  ) : (
     <>
-      <p>{name}</p>
+      <p>{profile.name}</p>
       <button onClick={() => solidApi.bookmark(page)}>Clip it!</button>
     </>
   );
