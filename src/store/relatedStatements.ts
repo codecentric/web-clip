c8f768352f20e47d6d74550c4bd937681ae44930
Angelo Veltens <angelo.veltens@codecentric.de>
2021-03-17 13:50:18 +0100

    refactor

--- src/store/relatedStatements.ts
+++ src/store/relatedStatements.ts
@@ -5,19 +5,39 @@ export function relatedStatements(
   pageUrl: NamedNode,
   targetDocument: NamedNode
 ): Statement[] {
-  const statements = store
+  const related = mapPageStatementsToTargetDocument(
+    store,
+    pageUrl,
+    targetDocument
+  );
+
+  const about = createAboutStatements(pageUrl, related, targetDocument);
+  return [...about, ...related];
+}
+
+function mapPageStatementsToTargetDocument(
+  store: IndexedFormula,
+  pageUrl: NamedNode,
+  targetDocument: NamedNode
+) {
+  return store
     .statementsMatching(null, null, null, pageUrl)
     .map((it: Statement) => {
       return st(it.subject, it.predicate, it.object, targetDocument);
     });
-  if (statements.length === 0) {
-    return [];
-  }
-  const uniqueSubjects = Array.from(
-    new Set(statements.map((it) => it.subject))
-  );
-  const about = uniqueSubjects.map((it) =>
+}
+
+function createAboutStatements(
+  pageUrl: NamedNode,
+  statements: Statement[],
+  targetDocument: NamedNode
+) {
+  const uniqueSubjects = findUniqueSubjects(statements);
+  return uniqueSubjects.map((it) =>
     st(pageUrl, sym('http://schema.org/about'), it, targetDocument)
   );
-  return [...about, ...statements];
+}
+
+function findUniqueSubjects(statements: Statement[]) {
+  return Array.from(new Set(statements.map((it) => it.subject)));
 }
