5149866f74c5fb6214e3a9b1cae7bbf6e34ddce6
Angelo Veltens <angelo.veltens@codecentric.de>
2021-09-10 15:26:11 +0200

    remove unused imports

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,8 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
-const baseManifest = require('./chrome/manifest.json');
-const WebpackExtensionManifestPlugin = require('webpack-extension-manifest-plugin');
 const config = {
   mode: 'development',
   devtool: 'cheap-module-source-map',
