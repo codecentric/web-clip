107f18810d52e396589ffb343ed0db4057fa99a1
Angelo Veltens <angelo.veltens@codecentric.de>
2022-02-12 20:58:25 +0100

    handle login error

--- src/content/useLogin.spec.ts
+++ src/content/useLogin.spec.ts
@@ -1,4 +1,5 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { act, renderHook } from '@testing-library/react-hooks';
+import { when } from 'jest-when';
 import { MessageType } from '../messages';
 import { sendMessage } from './sendMessage';
 import { useLogin } from './useLogin';
@@ -10,5 +11,17 @@ describe('useLogin', () => {
     const { result } = renderHook(() => useLogin());
     await result.current.login();
     expect(sendMessage).toHaveBeenCalledWith({ type: MessageType.LOGIN });
+  });
+  it('returns error, when login failed', async () => {
+    when(sendMessage)
+      .calledWith({ type: MessageType.LOGIN })
+      .mockRejectedValue(new Error('something went wrong'));
+    const { result } = renderHook(() => useLogin());
+    await act(async () => {
+      await result.current.login();
+    });
+    expect(result.all[1]).toMatchObject({
+      error: 'something went wrong',
+    });
   });
 });
