949e8329210c4607eb9a5e8fbabc5a9723e5c624
Angelo Veltens <angelo.veltens@codecentric.de>
2022-02-12 20:58:25 +0100

    handle login error

--- src/content/useLogin.ts
+++ src/content/useLogin.ts
@@ -1,12 +1,19 @@
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 import { MessageType } from '../messages';
 import { sendMessage } from './sendMessage';
 
 export const useLogin = () => {
+  const [error, setError] = useState(null);
+
   const login = useCallback(async () => {
-    await sendMessage({ type: MessageType.LOGIN });
+    try {
+      await sendMessage({ type: MessageType.LOGIN });
+    } catch (error) {
+      setError(error.message);
+    }
   }, []);
   return {
     login,
+    error,
   };
 };
