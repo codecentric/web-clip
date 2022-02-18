9f39c540ac3195b98e52d7843b1abbeaacba81f0
Angelo Veltens <angelo.veltens@codecentric.de>
2022-02-18 15:58:05 +0100

    send logged in message to active tab

--- src/background.ts
+++ src/background.ts
@@ -1,11 +1,15 @@
 import { SolidApi } from './api/SolidApi';
 import { activateWebClipForTab } from './background/activate';
 import { MessageHandler } from './background/MessageHandler';
+import { sendMessageToActiveTab } from './background/sendMessageToActiveTab';
+import { MessageType } from './messages';
 import { Session } from './solid-client-authn-chrome-ext/Session';
 import { Store } from './store/Store';
 
+const session = new Session();
+
 const solidApi = new SolidApi(
-  new Session(),
+  session,
   new Store(),
   chrome.identity.getRedirectURL()
 );
@@ -19,4 +23,11 @@ chrome.browserAction.onClicked.addListener(function (tab) {
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   messageHandler.handleMessage(request, sender).then(sendResponse);
   return true; // indicate async response
+});
+
+session.onLogin(() => {
+  sendMessageToActiveTab({
+    type: MessageType.LOGGED_IN,
+    payload: session.info,
+  });
 });
