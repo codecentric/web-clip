b62ad8164f73bf140e6e13a2add485e69b7f007a
Angelo Veltens <angelo.veltens@codecentric.de>
2022-02-18 16:33:24 +0100

    send message to background script to load profile

---
+++ src/content/useProfile.spec.ts
@@ -0,0 +1,54 @@
+import { act, renderHook } from '@testing-library/react-hooks';
+import { when } from 'jest-when';
+import { MessageType } from '../messages';
+import { sendMessage } from './sendMessage';
+import { useProfile } from './useProfile';
+
+jest.mock('./sendMessage');
+
+describe('useProfile', () => {
+  it('it returns a loading status while the profile is loading', () => {
+    when(sendMessage)
+      .calledWith({
+        type: MessageType.LOAD_PROFILE,
+      })
+      .mockReturnValue(new Promise(() => null));
+
+    const { result } = renderHook(() => useProfile());
+
+    act(() => {
+      expect(sendMessage).toHaveBeenCalledWith({
+        type: MessageType.LOAD_PROFILE,
+      });
+      expect(result.all[0]).toEqual({
+        loading: true,
+      });
+      expect(result.all).toHaveLength(1);
+    });
+  });
+
+  it('returns the profile once it has been loaded', async () => {
+    when(sendMessage)
+      .calledWith({
+        type: MessageType.LOAD_PROFILE,
+      })
+      .mockResolvedValue({
+        name: 'Jane Doe',
+      });
+
+    const { result, waitForNextUpdate } = renderHook(() => useProfile());
+
+    await waitForNextUpdate();
+
+    act(() => {
+      expect(result.all).toHaveLength(2);
+      expect(result.all[0]).toEqual({
+        loading: true,
+      });
+      expect(result.all[1]).toEqual({
+        loading: false,
+        profile: { name: 'Jane Doe' },
+      });
+    });
+  });
+});
--- src/messages.ts
+++ src/messages.ts
@@ -2,6 +2,7 @@ export enum MessageType {
   ACTIVATE = 'ACTIVATE',
   LOGIN = 'LOGIN',
   LOGGED_IN = 'LOGGED_IN',
+  LOAD_PROFILE = 'LOAD_PROFILE',
 }
 
 export type Message = {
