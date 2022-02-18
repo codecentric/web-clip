9af3d2bc821c201035d026ef6530cd629832fe1d
Angelo Veltens <angelo.veltens@codecentric.de>
2022-02-18 17:35:23 +0100

    add useBookmark hook that sends messages to background script

--- src/messages.ts
+++ src/messages.ts
@@ -3,6 +3,8 @@ export enum MessageType {
   LOGIN = 'LOGIN',
   LOGGED_IN = 'LOGGED_IN',
   LOAD_PROFILE = 'LOAD_PROFILE',
+  LOAD_BOOKMARK = 'LOAD_BOOKMARK',
+  ADD_BOOKMARK = 'ADD_BOOKMARK',
 }
 
 export type Message = {
