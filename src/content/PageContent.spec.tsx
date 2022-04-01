8201b3d85ed5f7443cabb629d81fd705325e8b9c
Angelo Veltens <angelo.veltens@codecentric.de>
2022-04-01 14:21:39 +0200

    remove obsolete hook

--- src/content/PageContent.spec.tsx
+++ src/content/PageContent.spec.tsx
@@ -2,20 +2,14 @@ import { ISessionInfo } from '@inrupt/solid-client-authn-browser';
 import { render, screen } from '@testing-library/react';
 import React from 'react';
 import { PageContent } from './PageContent';
-import { useSolid } from './useSolid';
 import { useSessionInfo } from './useSessionInfo';
 
-jest.mock('./useSolid');
 jest.mock('./useSessionInfo');
 
 describe('PageContent', () => {
   beforeEach(() => {
     (useSessionInfo as jest.Mock).mockReturnValue({
       isLoggedIn: false,
-    });
-    (useSolid as jest.Mock).mockReturnValue({
-      store: null,
-      solidApi: null,
     });
   });
   describe('close icon', () => {
