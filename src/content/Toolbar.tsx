4e2cd1621604f4c2a29a7dd115785b03b6a75615
Angelo Veltens <angelo.veltens@codecentric.de>
2022-04-08 18:57:11 +0200

    use button component for clip button

--- src/content/Toolbar.tsx
+++ src/content/Toolbar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button } from '../components/Button';
 import { ErrorMessage } from '../components/ErrorMessage';
 import { useBookmark } from './useBookmark';
 import { usePage } from './usePage';
@@ -12,18 +13,17 @@ interface Props {
 export const Toolbar = ({ profile }: Props) => {
   const page = usePage();
   const { addBookmark, loading, saving, error, bookmark } = useBookmark(page);
-  const waiting = loading || saving;
   return (
     <>
       <p className="my-1">{profile.name}</p>
       {loading ? null : (
-        <button
-          className="my-1 px-4 py-2 bg-blue-400 rounded text-white hover:opacity-90 font-bold"
-          disabled={waiting}
+        <Button
+          loading={saving}
+          loadingLabel="Saving..."
           onClick={() => addBookmark()}
         >
-          {saving ? 'Saving...' : 'Clip it!'}
-        </button>
+          Clip it!
+        </Button>
       )}
       {error && <ErrorMessage error={error} />}
       {bookmark && (
