0d21e445bcf18da2eb262398b3326a4e6128a45e
Angelo Veltens <angelo.veltens@codecentric.de>
2022-04-08 18:24:45 +0200

    introduce loading state for login hook

--- src/content/useLogin.spec.ts
+++ src/content/useLogin.spec.ts
@@ -7,12 +7,40 @@ import { useLogin } from './useLogin';
 jest.mock('./sendMessage');
 
 describe('useLogin', () => {
+  it('returns loading false', () => {
+    const { result } = renderHook(() => useLogin());
+    expect(result.current).toMatchObject({
+      loading: false,
+    });
+  });
+
   it('sends message to the background script on login', async () => {
     const { result } = renderHook(() => useLogin());
     await result.current.login();
     expect(sendMessage).toHaveBeenCalledWith({ type: MessageType.LOGIN });
   });
-  it('returns error, when login failed', async () => {
+
+  it('indicates loading and no error while logging in', async () => {
+    const { result } = renderHook(() => useLogin());
+    await act(async () => {
+      await result.current.login();
+    });
+    expect(result.all).toHaveLength(3);
+    expect(result.all[0]).toMatchObject({
+      loading: false,
+      error: null,
+    });
+    expect(result.all[1]).toMatchObject({
+      loading: true,
+      error: null,
+    });
+    expect(result.all[2]).toMatchObject({
+      loading: false,
+      error: null,
+    });
+  });
+
+  it('returns error and stops loading, when login failed', async () => {
     when(sendMessage)
       .calledWith({ type: MessageType.LOGIN })
       .mockRejectedValue(new Error('something went wrong'));
@@ -20,7 +48,16 @@ describe('useLogin', () => {
     await act(async () => {
       await result.current.login();
     });
+    expect(result.all[0]).toMatchObject({
+      loading: false,
+      error: null,
+    });
     expect(result.all[1]).toMatchObject({
+      loading: true,
+      error: null,
+    });
+    expect(result.all[2]).toMatchObject({
+      loading: false,
       error: new Error('something went wrong'),
     });
   });
