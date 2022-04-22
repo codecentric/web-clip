5464a2cb661c013ae36c0ee4cbcf056b80c846ed
Angelo Veltens <angelo.veltens@codecentric.de>
2022-04-22 18:32:57 +0200

    login button for options page

---
+++ src/options/connect-pod/LoginButton.spec.tsx
@@ -0,0 +1,28 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import React from 'react';
+import { LoginButton } from './LoginButton';
+import { useLogin } from './useLogin';
+
+jest.mock('./useLogin');
+
+describe('LoginButton', () => {
+  it('triggers login when clicked', () => {
+    const login = jest.fn();
+    (useLogin as jest.Mock).mockReturnValue({
+      login,
+    });
+    render(<LoginButton />);
+    const button = screen.getByText('Login');
+    fireEvent.click(button);
+    expect(login).toHaveBeenCalled();
+  });
+
+  it('indicates loading', () => {
+    (useLogin as jest.Mock).mockReturnValue({
+      error: null,
+      loading: true,
+    });
+    render(<LoginButton />);
+    expect(screen.getByText('Signing in')).toBeInTheDocument();
+  });
+});
---
+++ src/options/connect-pod/useLogin.ts
@@ -0,0 +1,6 @@
+export const useLogin = () => ({
+  loading: false,
+  login: () => {
+    throw new Error('not implemented');
+  },
+});
