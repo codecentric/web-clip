c623519da4481394f9255ee9e45ac9aea4f51043
Angelo Veltens <angelo.veltens@codecentric.de>
2022-04-27 06:57:03 +0200

    extract GetAPodSection

---
+++ src/options/get-a-pod/GetAPodSection.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export const GetAPodSection = () => (
+  <section>
+    <h2 className="text-lg font-medium my-8">1) Get a Solid Pod</h2>
+    <p className="my-4">
+      For information on how to create your own pod, please visit{' '}
+      <a
+        className="text-blue-600 hover:underline"
+        href="https://solidproject.org/users/get-a-pod"
+      >
+        the Solid Project
+      </a>{' '}
+      or just create a pod on{' '}
+      <a
+        className="text-blue-600 hover:underline"
+        href="https://solidcommunity.net/register"
+      >
+        solidcommunity.net
+      </a>
+    </p>
+  </section>
+);
