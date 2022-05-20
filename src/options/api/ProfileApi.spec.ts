e1bb3736c8575a1b8b6b5c0e7f58b601070a0321
Angelo Veltens <angelo.veltens@codecentric.de>
2022-05-20 19:32:37 +0200

    grant access by inserting trusted app tripes to user profile

--- src/options/api/ProfileApi.spec.ts
+++ src/options/api/ProfileApi.spec.ts
@@ -1,6 +1,7 @@
 import { when } from 'jest-when';
 import { Fetcher, graph, LiveStore, UpdateManager } from 'rdflib';
 import { Session } from '../../solid-client-authn-chrome-ext/Session';
+import { thenSparqlUpdateIsSentToUrl } from '../../test/thenSparqlUpdateIsSentToUrl';
 import { ProfileApi } from './ProfileApi';
 
 describe('ProfileApi', () => {
@@ -61,6 +62,39 @@ describe('ProfileApi', () => {
         'chrome-extension://extension-id'
       );
       expect(result).toBe(true);
+    });
+  });
+
+  describe('grantAccessTo', () => {
+    it('adds extension URL as trusted ap', async () => {
+      const fetchMock = jest.fn();
+      when(fetchMock)
+        .calledWith('https://pod.test/alice', expect.anything())
+        .mockResolvedValue(turtleResponse(''));
+      const session = {
+        info: {
+          webId: 'https://pod.test/alice#me',
+        },
+      } as Session;
+      const updater = new UpdateManager();
+      const fetcher = new Fetcher(updater.store, { fetch: fetchMock });
+      const profileApi = new ProfileApi(session, fetcher.store as LiveStore);
+      await profileApi.grantAccessTo('chrome://extension-id');
+      thenSparqlUpdateIsSentToUrl(
+        fetchMock,
+        'https://pod.test/alice',
+        `
+      INSERT DATA {
+        <https://pod.test/alice#me> <http://www.w3.org/ns/auth/acl#trustedApp> <https://pod.test/alice#trust-webclip> .
+        <https://pod.test/alice#trust-webclip>
+          <http://www.w3.org/ns/auth/acl#origin> <chrome://extension-id> ;
+          <http://www.w3.org/ns/auth/acl#mode>
+            <http://www.w3.org/ns/auth/acl#Read>,
+            <http://www.w3.org/ns/auth/acl#Write>,
+            <http://www.w3.org/ns/auth/acl#Append> ;
+         .
+      }`
+      );
     });
   });
 });
