47311df654287e638c622d918305e8fde84dfd52
Angelo Veltens <angelo.veltens@codecentric.de>
2022-05-27 19:02:09 +0200

    auto-login on permission page

--- src/content/authorization-page/useAuthorization.ts
+++ src/content/authorization-page/useAuthorization.ts
@@ -1,13 +1,33 @@
 import { Session } from '@inrupt/solid-client-authn-browser';
+import { useEffect, useState } from 'react';
 
 export const useAuthorization = (
   session: Session,
   providerUrl: string,
   extensionId: string
 ) => {
-  return {
-    loading: false,
-    success: true,
-    error: new Error('todo: implement error handling'),
-  };
+  const [state, setState] = useState({
+    loading: true,
+    success: false,
+    error: null,
+  });
+
+  useEffect(() => {
+    if (!session.info.isLoggedIn) {
+      session
+        .login({
+          oidcIssuer: providerUrl,
+        })
+        .then(() => console.log('login via redirect'));
+    } else {
+      setState((currentState) => ({
+        ...currentState,
+        loading: false,
+        success: false,
+        error: new Error('todo: grant permissions'),
+      }));
+    }
+  }, []);
+
+  return state;
 };
