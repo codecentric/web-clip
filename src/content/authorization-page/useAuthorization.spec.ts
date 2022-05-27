af060c7c49c5f59a3f16bf12ce4992854979a8ee
Angelo Veltens <angelo.veltens@codecentric.de>
2022-05-27 19:28:19 +0200

    grant access to extension id

--- src/content/authorization-page/useAuthorization.spec.ts
+++ src/content/authorization-page/useAuthorization.spec.ts
@@ -1,6 +1,11 @@
 import { Session } from '@inrupt/solid-client-authn-browser';
 import { renderHook, RenderResult } from '@testing-library/react-hooks';
+import { when } from 'jest-when';
+import { ProfileApi } from '../../options/api/ProfileApi';
+import { useSolidApis } from '../../options/useSolidApis';
 import { useAuthorization } from './useAuthorization';
+
+jest.mock('../../options/useSolidApis');
 
 describe('useAuthorization', () => {
   let session: Session;
@@ -14,6 +19,9 @@ describe('useAuthorization', () => {
         },
         login: jest.fn().mockResolvedValue(null),
       } as unknown as Session;
+      const profileApi = {} as ProfileApi;
+      when(useSolidApis).calledWith(session).mockReturnValue({ profileApi });
+
       const render = renderHook(() =>
         useAuthorization(session, 'https://pod.test', 'extension-id')
       );
@@ -32,7 +40,8 @@ describe('useAuthorization', () => {
     });
   });
 
-  describe('when already logged in', () => {
+  describe('when already logged in and granting access succeeds', () => {
+    let profileApi: ProfileApi;
     beforeEach(() => {
       session = {
         info: {
@@ -40,18 +49,68 @@ describe('useAuthorization', () => {
         },
         login: jest.fn(),
       } as unknown as Session;
+      profileApi = {
+        grantAccessTo: jest.fn().mockResolvedValue(null),
+      } as unknown as ProfileApi;
+      when(useSolidApis).calledWith(session).mockReturnValue({ profileApi });
       const render = renderHook(() =>
         useAuthorization(session, 'https://pod.test', 'extension-id')
       );
       renderResult = render.result;
     });
 
-    it('indicates no loading', () => {
+    it('stops loading when finished', () => {
+      expect(renderResult.all[0]).toMatchObject({ loading: true });
       expect(renderResult.current.loading).toBe(false);
     });
 
     it('does not trigger another login', () => {
       expect(session.login).not.toHaveBeenCalled();
+    });
+
+    it('grants permissions to the extension url', () => {
+      expect(profileApi.grantAccessTo).toHaveBeenCalledWith(
+        'chrome-extension://extension-id'
+      );
+    });
+
+    it('indicates success', () => {
+      expect(renderResult.current.success).toBe(true);
+      expect(renderResult.current.error).toBe(null);
+    });
+  });
+
+  describe('when already logged in, but granting access fails', () => {
+    let profileApi: ProfileApi;
+    beforeEach(() => {
+      session = {
+        info: {
+          isLoggedIn: true,
+        },
+        login: jest.fn(),
+      } as unknown as Session;
+      profileApi = {
+        grantAccessTo: jest
+          .fn()
+          .mockRejectedValue(new Error('granting access failed')),
+      } as unknown as ProfileApi;
+      when(useSolidApis).calledWith(session).mockReturnValue({ profileApi });
+      const render = renderHook(() =>
+        useAuthorization(session, 'https://pod.test', 'extension-id')
+      );
+      renderResult = render.result;
+    });
+
+    it('stops loading when finished', () => {
+      expect(renderResult.all[0]).toMatchObject({ loading: true });
+      expect(renderResult.current.loading).toBe(false);
+    });
+
+    it('indicates error', () => {
+      expect(renderResult.current.success).toBe(false);
+      expect(renderResult.current.error).toEqual(
+        new Error('granting access failed')
+      );
     });
   });
 });
