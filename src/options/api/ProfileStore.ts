5546cf8c572b3f95d57d56404f5c94907dacf81a
Angelo Veltens <angelo.veltens@codecentric.de>
2022-06-03 16:31:17 +0200

    check if profile is pointing to an origin

--- src/options/api/ProfileStore.ts
+++ src/options/api/ProfileStore.ts
@@ -13,6 +13,18 @@ export class ProfileStore {
   }
 
   /**
+   * Checks whether the profile document of the given WebID
+   * holds any statement pointing to the given origin
+   *
+   * @param webId The Web ID of the user, whose profile document is checked
+   * @param origin The origin that is looked for
+   */
+  holdsOrigin(webId: string, origin: string) {
+    const profileDoc = sym(webId).doc();
+    return this.store.holds(null, acl('origin'), sym(origin), profileDoc);
+  }
+
+  /**
    * Checks whether the user identified by the given Web ID has granted all the
    * required permissions to the WebClip extension identified by the given origin
    *
