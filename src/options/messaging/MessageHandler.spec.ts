1bccf1f72d0953ac66e1861caa9dc3816865b80f
Angelo Veltens <angelo.veltens@codecentric.de>
2022-06-03 15:08:14 +0200

    close tab after granting access

--- src/options/messaging/MessageHandler.spec.ts
+++ src/options/messaging/MessageHandler.spec.ts
@@ -1,22 +1,41 @@
-import { MessageType } from '../../messages';
-import { ActionType } from '../reducer';
+import { closeTab } from '../../chrome/closeTab';
+import { MessageType, Response } from '../../messages';
+import { ActionType, Dispatch } from '../reducer';
 import { MessageHandler } from './MessageHandler';
+
+jest.mock('../../chrome/closeTab');
 
 describe('MessageHandler', () => {
   describe('when ACCESS_GRANTED handled', () => {
-    it('it dispatches trusted app action and returns nothing', async () => {
-      const dispatch = jest.fn();
+    let dispatch: Dispatch;
+    let result: Response;
+    beforeEach(async () => {
+      dispatch = jest.fn();
       const handler = new MessageHandler(dispatch);
-      const result = await handler.handleMessage(
+      result = await handler.handleMessage(
         {
           type: MessageType.ACCESS_GRANTED,
         },
-        {}
+        {
+          tab: {
+            id: 123456,
+          } as chrome.tabs.Tab,
+        }
       );
-      expect(result).toEqual({});
+    });
+
+    it('it dispatches trusted app action', async () => {
       expect(dispatch).toHaveBeenCalledWith({
         type: ActionType.TRUSTED_APP,
       });
+    });
+
+    it('it returns nothing', async () => {
+      expect(result).toEqual({});
+    });
+
+    it('it closes the sending tab', async () => {
+      expect(closeTab).toHaveBeenCalledWith(123456);
     });
   });
 });
