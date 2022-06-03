8d2929322001e7033965a4c87babd30e8d02d09a
Angelo Veltens <angelo.veltens@codecentric.de>
2022-06-03 16:31:17 +0200

    check if profile is pointing to an origin

--- src/options/api/ProfileStore.spec.ts
+++ src/options/api/ProfileStore.spec.ts
@@ -2,6 +2,97 @@ import { graph, parse } from 'rdflib';
 import { ProfileStore } from './ProfileStore';
 
 describe('ProfileStore', () => {
+  describe('holdsOrigin', () => {
+    it('returns false if store is empty', () => {
+      const store = graph();
+      const profileStore = new ProfileStore(store);
+      const result = profileStore.holdsOrigin(
+        'https://pod.test/alice#me',
+        'https://extension-id.chromiumapp.org'
+      );
+      expect(result).toBe(false);
+    });
+
+    it('returns false if store contains trusted app, but not for the given origin', () => {
+      const store = graph();
+      parse(
+        `
+        @prefix acl: <http://www.w3.org/ns/auth/acl#> .
+        <#me> acl:trustedApp [
+          acl:origin <https://wrong-id.chromiumapp.org> ;
+          acl:mode acl:Read, acl:Write, acl:Append ;
+        ] .
+        `,
+        store,
+        'https://pod.test/alice'
+      );
+      const profileStore = new ProfileStore(store);
+      const result = profileStore.holdsOrigin(
+        'https://pod.test/alice#me',
+        'https://extension-id.chromiumapp.org'
+      );
+      expect(result).toBe(false);
+    });
+
+    it('returns true if store contains trusted app with the given origin', () => {
+      const store = graph();
+      parse(
+        `
+        @prefix acl: <http://www.w3.org/ns/auth/acl#> .
+        <#me> acl:trustedApp [
+          acl:origin <https://extension-id.chromiumapp.org> ;
+        ] .
+        `,
+        store,
+        'https://pod.test/alice'
+      );
+      const profileStore = new ProfileStore(store);
+      const result = profileStore.holdsOrigin(
+        'https://pod.test/alice#me',
+        'https://extension-id.chromiumapp.org'
+      );
+      expect(result).toBe(true);
+    });
+
+    it('even returns true, if anything points to the origin', () => {
+      const store = graph();
+      parse(
+        `
+        @prefix acl: <http://www.w3.org/ns/auth/acl#> .
+        <> acl:origin <https://extension-id.chromiumapp.org> .
+        `,
+        store,
+        'https://pod.test/alice'
+      );
+      const profileStore = new ProfileStore(store);
+      const result = profileStore.holdsOrigin(
+        'https://pod.test/alice#me',
+        'https://extension-id.chromiumapp.org'
+      );
+      expect(result).toBe(true);
+    });
+
+    it('returns false if trusted app statements are found outside of the WebID profile doc', () => {
+      const store = graph();
+      parse(
+        `
+        @prefix acl: <http://www.w3.org/ns/auth/acl#> .
+        <https://pod.test/alice#me> acl:trustedApp [
+          acl:origin <https://extension-id.chromiumapp.org> ;
+        ] .
+        `,
+        store,
+        'https://pod.test/bob'
+      );
+      const profileStore = new ProfileStore(store);
+      const result = profileStore.holdsOrigin(
+        'https://pod.test/alice#me',
+        'https://extension-id.chromiumapp.org'
+      );
+      expect(result).toBe(false);
+    });
+  });
+
   describe('checkAccessPermissions', () => {
     it('returns false if store is empty', () => {
       const store = graph();
