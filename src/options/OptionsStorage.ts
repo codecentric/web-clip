8246bf5ba1b1adf9601504b3031e1009d154137a
Angelo Veltens <angelo.veltens@codecentric.de>
2022-06-27 13:42:02 +0200

    emit events when options change; log out when provider url changes

--- src/options/OptionsStorage.ts
+++ src/options/OptionsStorage.ts
@@ -1,8 +1,20 @@
+import { EventEmitter } from 'events';
 import { load, onChanged, Options } from './optionsStorageApi';
 
-export class OptionsStorage {
+/**
+ * Provides access to the current values of the extension options.
+ * Emits an event with the same name as the option, when an option changes.
+ */
+export class OptionsStorage extends EventEmitter {
   private value: Options;
 
+  constructor() {
+    super();
+  }
+
+  /**
+   * Initially loads the option values from storage
+   */
   async init() {
     this.value = await load();
     this.updateWhenChanged();
@@ -11,13 +23,15 @@ export class OptionsStorage {
   private updateWhenChanged() {
     onChanged((changes, namespace) => {
       if (namespace !== 'sync') return;
-      const result = Object.keys(changes).reduce(
+      const keys = Object.keys(changes);
+      const result = keys.reduce(
         (acc, value) => ({
           ...acc,
           [value]: changes[value].newValue,
         }),
         {}
       );
+      keys.forEach((key) => this.emit(key, changes[key]));
       this.value = {
         ...this.value,
         ...result,
@@ -25,6 +39,9 @@ export class OptionsStorage {
     });
   }
 
+  /**
+   * The current option values
+   */
   getOptions(): Options {
     return this.value;
   }
